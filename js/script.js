c96236004d3751040d9cd85a45756fbc423c52cf
Shuvojeet Das <64420378+shuvojeetdas842@users.noreply.github.com>
2020-05-10 23:23:44 +0530

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var names=["Mark","John","Leo","Jack","jean","Chris","jackson","watson"];
+for(var i=0;i<names.length;i++){
+	if(names[i][0]=="J" || names[i][0]=="j"){
+		console.log("Goodbye "+names[i]);
+	}
+	else{
+		console.log("Hello "+names[i]);
+	}
+}
